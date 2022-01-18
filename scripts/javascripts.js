ed3d097d39af335bf2e985b76b90a40dfcd36468
claes <claessh@yahoo.no>
2022-01-18 20:02:03 +0100

    added loop to play a game of 5 rounds

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -1,38 +1,37 @@
-
-
-//Get the computer selection
-let computerSelection = computerPlay();
-console.log(computerSelection);
-//let user choose rps
-let playerSelection = prompt('Enter rock or paper or scissor').toLowerCase();
-console.log(playerSelection);
-
-console.log(playRound(playerSelection,computerSelection));
+console.log(game());
 
 //Create afunction playRound to play rps with computer
 function playRound(playerSelection,computerSelection){
     //Declare outcome string
     let outcome;
     console.log(computerSelection)
+    console.log(playerSelection)
     //compare what user and computer selected
     if(playerSelection==='rock'&&computerSelection==='paper'){
-        return 'You loose! Paper beats Rock';
+        console.log('You loose! Paper beats Rock');
+        return 'comp';
     } else if(playerSelection==='rock'&&computerSelection==='scissor'){
-        return 'You win! Rock beats Scissor'; 
+        console.log('You win! Rock beats Scissor'); 
+        return 'you';
     }
     else if(playerSelection==='paper'&&computerSelection==='scissor'){
-        return 'You loose! Scissor beat Paper'; 
+        console.log('You loose! Scissor beat Paper');
+        return 'comp'; 
     }
     else if(playerSelection==='paper'&&computerSelection==='rock'){
-        return 'You win! Paper beats Rock'; 
+        console.log('You win! Paper beats Rock'); 
+        return 'you';
     }
     else if(playerSelection==='scissor'&&computerSelection==='rock'){
-        return 'You loose! Rock beats Scissor'; 
+        console.log('You loose! Rock beats Scissor'); 
+        return 'comp';
     }
     else if(playerSelection==='scissor'&&computerSelection==='paper'){
-        return 'You win! Scissor beats Paper'; 
+        console.log('You win! Scissor beats Paper'); 
+        return 'you';
     } else{
-        return 'DRAW';
+        console.log('DRAW');
+        return 'draw';
     }
 }
 
@@ -51,3 +50,40 @@ function computerPlay(){
         return 'scissor';
     }
 }
+
+//a function game that will play 5 rounds of rps
+function game(){
+
+    //Variables for the player scores
+    let yourScore = 0;
+    let compScore = 0;
+    let computerSelection;
+    let playerSelection;
+    let outcome;
+    //loop through playRound five times
+    for(i = 1; i <=5; i++){
+        //Get the computer selection
+        computerSelection = computerPlay();
+        //let user choose rps
+        playerSelection = prompt('Enter rock or paper or scissor').toLowerCase();
+        outcome = playRound(playerSelection,computerSelection);
+        //update score depending of outcome
+        if(outcome === 'you'){
+            yourScore += 1;
+        } else if(outcome === 'comp'){
+            compScore += 1;
+        }        
+    }
+    
+    //return the winner of the 5 rounds
+    if(yourScore > compScore){
+        return 'YOU ARE THE WINNER';
+    } else if(compScore > yourScore){
+        return 'You looose :('
+    } else{
+        return 'draaaw...'
+    }
+}
+
+
+
