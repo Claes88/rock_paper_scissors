460242cb516ff5a920f6798441de516145172ef0
claes <claessh@yahoo.no>
2022-07-21 11:17:05 +0200

    Added comments and removed old code

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -1,9 +1,19 @@
-//Create afunction playRound to play rps with computer
-const div = document.querySelector('div'); 
-div.style.color = 'blue';
+//Elements that will be changed in the rps 
+const divYou = document.querySelector("#yourscore");
+const divComp = document.querySelector("#compscore");
+const hYou = document.createElement('h2');
+const hComp = document.createElement('h2');
+
+const div = document.querySelector('#outcome'); 
+div.style.color = '#232B2B';
 const p = document.createElement('p');
+
 let yourScore = 0;
 let compScore = 0;
+
+displayScore(yourScore,compScore);
+
+//function to update the score based on selection
 function playRound(playerSelection,computerSelection){
     
     let outcome;
@@ -11,54 +21,57 @@ function playRound(playerSelection,computerSelection){
     console.log(playerSelection)
     //compare what user and computer selected
     if(playerSelection==='rock'&&computerSelection==='paper'){
-        //displayOutcome('You loose! Paper beats Rock');
-        //return 'comp';
+        displayOutcome('  +1 to Computer! Paper beats Rock');
         compScore += 1; 
     } else if(playerSelection==='rock'&&computerSelection==='scissor'){
-        //displayOutcome('You win! Rock beats Scissor');
-        //return 'you';
+        displayOutcome('  +1 to you! Rock beats Scissor');
         yourScore += 1;
     }
     else if(playerSelection==='paper'&&computerSelection==='scissor'){
-        //displayOutcome('You loose! Scissor beat Paper');
-        //return 'comp'; 
+        displayOutcome('  +1 to Computer! Scissor beat Paper');
         compScore += 1;
     } 
     else if(playerSelection==='paper'&&computerSelection==='rock'){
-        //displayOutcome('You win! Paper beats Rock'); 
-        //return 'you';
+        displayOutcome('  +1 to you!! Paper beats Rock'); 
         yourScore += 1;
     }
     else if(playerSelection==='scissor'&&computerSelection==='rock'){
-        //displayOutcome('You loose! Rock beats Scissor');         
-        //return 'comp';
+        displayOutcome('  +1 to Computer! Rock beats Scissor');         
         compScore += 1;
     }
     else if(playerSelection==='scissor'&&computerSelection==='paper'){
-        //displayOutcome('You win! Scissor beats Paper'); 
-        //return 'you';
+        displayOutcome('  +1 to you!! Scissor beats Paper'); 
         yourScore += 1;
     } else{
-       //displayOutcome('DRAW'); 
        yourScore = yourScore;
     }
 
     //display score
     if(yourScore === 5){
-        displayOutcome("You are the winner!!!" + ' Your score is: ' + yourScore + ' ComputerScore is: ' + compScore);
+        displayOutcome("You are the winner!!!");
     } else if (compScore === 5){
-        displayOutcome("You loose, computer wins!!!" + ' Your score is: ' + yourScore + ' ComputerScore is: ' + compScore);
-    } else {
-        displayOutcome('Your score is: ' + yourScore + ' ComputerScore is: ' + compScore);
+        displayOutcome("You loose, computer wins!!!");
     } 
+    
+    displayScore(yourScore,compScore);
+     
 }
 
+//displays the outcome of the round or potential winner
 function displayOutcome(text){
     p.textContent = text;
     div.append(p);
 }
 
-//A function computerPlay to decide computer outcome
+//displays current score
+function displayScore(yourScore, compScore){
+    hYou.textContent = yourScore;
+    hComp.textContent = compScore;
+    divYou.append(hYou);
+    divComp.append(hComp);
+}
+
+//A function to decide computer outcome
 function computerPlay(){
     //Generate a random number, only three different outcomes
     let number = Math.floor(Math.random()*3);
@@ -76,13 +89,11 @@ function eventOutcome(ev){
     console.log(ev);
 }
 
-
-
+//eventlistener for the buttons
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', event => {
         playRound(button.id,computerPlay());
-        //game();
     });  
 });
 
